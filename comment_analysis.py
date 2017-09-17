790ea34d242f8ce60e155015e5b0256f7f50b42e
Licui <licui0901@163.com>
2017-09-17 23:19:57 +0800

    function test

--- comment_analysis.py
+++ comment_analysis.py
@@ -7,6 +7,7 @@ from requests.exceptions import RequestException
 import jieba
 from gensim import corpora, models
 
+
 def parse_url(url, data):
     try:
         response = requests.get(url, params=data).text
@@ -15,12 +16,14 @@ def parse_url(url, data):
         return None
     return comments_json
 
+
 def create_dataframe(url, data):
     comments_json = parse_url(url, data)
     j = json.loads(comments_json)
     comments = j['comments']
     df_comments = pd.DataFrame(comments[0])
     return df_comments
+
 
 def append_dataframe(url, data, page, df_comment):
     data['page'] = page
@@ -31,6 +34,7 @@ def append_dataframe(url, data, page, df_comment):
         df_comment = df_comment.append(pd.Series(comment), ignore_index = True)
     return df_comment
 
+
 def get_comments(url, data, pages):
     df_comment = create_dataframe(url, data)
     for i in range(pages):
@@ -38,13 +42,15 @@ def get_comments(url, data, pages):
     df_content = df_comment['content']
     return df_content
 
+
 def mycut(s):
     return ' '.join(jieba.cut(s))
 
+
 def cut_sentence(df):
-    cut_1 = df[0].apply(mycut)
-    stoplist = 'stoplist.txt'
-    stop = pd.read_csv(stoplist, encoding = 'utf-8', header = None, sep = 'tipdm', engine='python')
+    cut_1 = df.apply(mycut)
+    stop_list = 'stoplist.txt'
+    stop = pd.read_csv(stop_list, encoding='utf-8', header=None, sep='tipdm', engine='python')
     # sep设置分割词，由于csv默认以半角逗号为分割词，而该词恰好在停用词表中，因此会导致读取出错
     # 所以解决办法是手动设置一个不存在的分割词，如tipdm。
     stop = [' ', ''] + list(stop[0])  # Pandas自动过滤了空格符，这里手动添加
@@ -53,12 +59,14 @@ def cut_sentence(df):
     # df_cut = pd.DataFrame(cut_3)
     return cut_3
 
+
 def print_topics(cut_3, num=50):
-    dict = corpora.Dictionary(cut_3) #建立词典
-    corpus = [dict.doc2bow(i) for i in cut_3] #建立语料库
-    lda = models.LdaModel(corpus, num_topics = num, id2word = dict) #LDA模型训练
-    for i in range(lda.num_topcis):
+    dicts = corpora.Dictionary(cut_3)  # 建立词典
+    corpus = [dicts.doc2bow(i) for i in cut_3]  # 建立语料库
+    lda = models.LdaModel(corpus, num_topics=num, id2word=dicts)  # LDA模型训练
+    for i in range(lda.num_topics):
         print(lda.print_topic(i))
+
 
 def main():
     url = 'https://club.jd.com/comment/productPageComments.action'
@@ -72,9 +80,9 @@ def main():
         'page': 0,
         'fold': 1
     }
-    df_content = get_comments(url, data, 100)
+    df_content = get_comments(url, data, 20)
     cut_content = cut_sentence(df_content)
-    print_topics(cut_content)
+    print_topics(cut_content, 10)
 
 if __name__ == '__main__':
     main()
